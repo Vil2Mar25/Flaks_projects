e183ec6b98b7b4602ec3dda8dc6a8591399d5f66
vilmar <vilmarlk@gmail.com>
2022-02-06 09:18:29 -0300

    parte2

---
+++ tutorial_routes.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask import Flask, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+   return "hello world"
+
+@app.route('/product/<name>')
+def get_product(name):
+  return "O produto é " + str(name)
+
+@app.route('/sale/<transaction_id>')
+def get_sale(transaction_id=0):
+  return "A movimentacao é "+str(transaction_id)
+
+@app.route('/create/<first_name>/<last_name>')
+def create(first_name=None, last_name=None):
+  return 'Ola ' + first_name + ',' + last_name
+
+@app.route('/dashboard/<name>')
+def dashboard(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('dashboard',name = user))
+   else:
+      user = request.args.get('name')
+      return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
